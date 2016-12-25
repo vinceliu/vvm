c9135c1117477bb29021a683b64e62301222a49c
Vincent Liu <vinceliu@gmail.com>
2016-12-25 08:05:04 +0000

    shellcheck.sh: no need sudo, caller is root

--- shellcheck.sh
+++ shellcheck.sh
@@ -2,9 +2,9 @@
 
 # Only run shellcheck in Linux
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse"
-    sudo apt-get -qq update
-    sudo apt-get -t trusty-backports install shellcheck
+    apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse"
+    apt-get -qq update
+    apt-get -t trusty-backports install shellcheck
 
     shellcheck shellcheck.sh build.sh
 fi
