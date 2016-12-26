f8569ed915661a29b2eab1427ffad7b02b03d7d4
Vincent Liu <vinceliu@gmail.com>
2016-12-26 13:51:51 +0000

    test: experiment in installing pthread deps

--- shellcheck.sh
+++ shellcheck.sh
@@ -5,6 +5,7 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse"
     apt-get -qq update
     apt-get -t trusty-backports install shellcheck
+    apt-get install libpthread libpthread libpthread-stubs0-dev
 
     shellcheck shellcheck.sh build.sh build_googletest.sh
 fi
